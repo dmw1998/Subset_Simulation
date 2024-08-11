fab448e707d336271ac2c523a6a602377636d252
Miaowen <66027205+dmw1998@users.noreply.github.com>
2024-08-11 17:40:39 +0000

    new file:   subs_sr_pde.py

---
+++ subs_sr_pde.py
@@ -0,0 +1,17 @@
+import numpy as np
+from fenics import *
+
+# Apply the subset simulation method with selective refinement strategy to the PDE model.
+
+# Generate the samples for the Gaussian random field.
+# We need to write a new method. Combine the subset simulation and the selective refinement strategy.
+
+# Solving the 1d poisson equation with the finite element method.
+def solving_pde(theta, l):
+    # input:
+    # theta: the parameter of the Gaussian random field
+    # l: the level -- related to mesh size
+
+    # output:
+    # u_h: the solution of the PDE
+
