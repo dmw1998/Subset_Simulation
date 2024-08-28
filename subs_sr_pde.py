e09a411f0de0236e7de88068db9f3a6f189f0734
dmw <miaowen.dong@tum.de>
2024-08-28 21:16:36 +0800

    Good result

--- subs_sr_pde.py
+++ subs_sr_pde.py
@@ -1,22 +1,10 @@
 import numpy as np
 from fenics import *
 
-# Apply the subset simulation method with selective refinement strategy to the PDE model.
-
-# Generate the samples for the Gaussian random field.
-# We need to write a new method. Combine the subset simulation and the selective refinement strategy.
-
-# Computing the coefficient for the pde
 def kl_expan(theta):
-    # input:
-    # theta: a numpy array of length M
-    
-    # output:
-    # a: a numpy array of length n
     
     M = len(theta)
     
-    # Define the spatial domain
     x = np.linspace(0, 1, 1000)
     
     beta = 1 / 0.01
@@ -43,19 +31,12 @@ def kl_expan(theta):
     
     return a_x
 
-# Solving the 1d poisson equation with the finite element method.
 def solving_pde(theta, l):
-    # input:
-    # theta: the parameter of the Gaussian random field
-    # l: the level -- related to mesh size
-
-    # output:
-    # u_h(1): the QoI on level l
-
+    
     import os
     os.environ["PETSC_OPTIONS"] = "-log_view ascii:out.log"
     
-    n_grid = 64 * 2 ** l
+    n_grid = int(32 * 2 ** l-1)
     a_x = kl_expan(theta)
     
     # Create the mesh and define function space
@@ -88,136 +69,121 @@ def solving_pde(theta, l):
     
     return 0.535 - u_h(1)
 
-def compute_cl(G, thetas_ls, N, p0, l, L):
-    # input:
-    # G: approximated IoQ
-    # thetas_ls: list of thetas
-    # N: number of samples
-    # p0: failure probability
-    # l: current level
-    # L: finest level
+def compute_cl(G, thetas, N, y, p_0, l, L):
     
-    # output:
-    # G, thetas_ls: updated G and thetas_ls
-    # c_l: failure level
-    
-    sorted_indices = sorted(range(N), key=lambda k: G[k])
+    sorted_indices = sorted(range(N), key=lambda k: G[k], reverse=False)
     sorted_G = [G[i] for i in sorted_indices]
-    sorted_theta_ls = [thetas_ls[i] for i in sorted_indices]
+    sorted_thetas = [thetas[i] for i in sorted_indices]
     
-    N0 = int(p0 * N)
-    c_l = sorted_G[N0-1]
+    N_0 = int(p_0 * N)
+    c_l = sorted_G[N_0-1]
     if c_l < 0 or l == L:
-        # When we reach the finest level
-        G = [g for g in G if g < 0]
+        G = [g for g in G if g >= 0]
     else:
-        G = sorted_G[:N0]
-        thetas_ls = sorted_theta_ls[:N0]
+        G = sorted_G[:N_0]
+        thetas = sorted_thetas[:N_0]
     
-    return G, thetas_ls, c_l
-    
-# Sampling the parameter theta follows the Gaussian distribution
-# Modified MH algorithm
-def generate_samples(theta, G, N, gamma, y, l, burn_in=0):
-    # input:
-    # theta: the initial parameter
-    # G: the initial QoI
-    # N: the number of samples
-    # gamma: the step size
-    # y: the threshold
-    # l: the current level
-    # burn_in: the burn-in period
+    return G, thetas, c_l
 
-    # output:
-    # theta: the generated samples
-    # G: the corresponding QoI
+def sampling(M, N, G, thetas, c_l, l, L, gamma, burn_in=0):
     
-    N0 = len(theta)
-    L_b = burn_in * N0
+    N0 = len(G)
+    L_b = burn_in * (N0 - 1)
+    cost = np.zeros(L)
+    
     for i in range(N + (burn_in - 1) * N0):
-        theta_new = 0.8 * theta[i] + np.sqrt(1 - 0.8 ** 2) * np.random.normal(0, 1)
-        G_new = solving_pde(theta_new, 0)
+        theta_new = 0.8 * thetas[i] + np.sqrt(1 - 0.8 ** 2) * np.random.normal(0, 1, M)
+        G_new = solving_pde(theta_new, 1)
+        cost[0] += 1
         tol = 1
-        for j in range(1, l):
-            if tol >= np.abs(G_new - y):
+        for j in range(2, l):
+            if tol >= np.abs(G_new - c_l):
                 tol *= gamma
                 G_new = solving_pde(theta_new, j)
+                cost[j-1] += 1
             else:
                 break
-            
-        if G_new <= y:
-            theta = np.append(theta, theta_new)
-            G = np.append(G, G_new)
+        
+        if G_new <= c_l:
+            thetas.append(theta_new)
+            G.append(G_new)
         else:
-            theta = np.append(theta, theta[i])
-            G = np.append(G, G[i])
+            thetas.append(thetas[i])
+            G.append(G[i])
             
-    return theta[L_b:], G[L_b:]
+    return thetas[L_b:], G[L_b:], cost
 
-# Apply the subset simulation method with selective refinement strategy to the PDE model.
-def mle_sr(gamma, y, p_0, N, L, burn_in):
+def mle_sr(gamma, y, p_0, M, N, L, burn_in):
     
-    N0 = int(p_0 * N)
+    l = 1
+    thetas = [np.random.normal(0, 1, M) for _ in range(N)]
+    G = [solving_pde(theta, 0) for theta in thetas]
+    cost = np.zeros(L)
+    cost[0] = N
+    print('cost =', cost)
     
-    l = 0
-    thetas = [np.random.normal(0, 1, 150) for _ in range(N)]
-    G = [solving_pde(theta, l) for theta in thetas]
+    G, thetas, c_1 = compute_cl(G, thetas, N, y, p_0, l, L)
+    print('c_1 = ', c_1)
+    # print('len(G) =', len(G))
     
-    cost = N      # The times of solving the PDE
+    l = 2
+    thetas, G, add_cost = sampling(M, N, G, thetas, c_1, l, L, gamma, burn_in=0)
+    cost += add_cost
+    print('cost =', cost)
+    # print('len(thetas) =', len(thetas))
+    # print('len(G) =', len(G))
     
-    # For l = 2, no burn-in
-    G, thetas, c_1 = compute_cl(G, thetas, N, p_0, 0, L)
-    print('c_2 = ', c_1)
+    G, thetas, c_2 = compute_cl(G, thetas, N, y, p_0, l, L)
+    print('c_2 = ', c_2)
     
-    thetas, G = generate_samples(thetas, G, N, gamma, c_1, 2)
+    _, G_2, add_cost = sampling(M, N, G, thetas, c_2, l, L, gamma, burn_in=0)
+    cost += add_cost
+    print('cost =', cost)
     
-    cost += N - N0
-    
-    c_2 = np.sort(G)[N0-1]
-    _, G_2 = generate_samples(thetas, G, N, gamma, c_2, 2)
-    
-    cost += N - N0
-    
-    mask = G_2 <= c_1
+    mask = np.array(G_2) <= c_1
     denominator = np.mean(mask)
+    print('denominator =', denominator)
     
     # For l > 2, burn-in
     c_l = c_2
-    for l in range(3,L):
+    for l in range(3, L):
         c_l_1 = c_l
-        thetas, G = generate_samples(thetas, G, N, gamma, c_l, l, burn_in)
-        cost += N + (burn_in - 1) * N0
-        c_l = np.sort(G)[N0-1]
+        thetas, G, add_cost = sampling(M, N, G, thetas, c_l, l, L, gamma, burn_in=burn_in)
+        cost += add_cost
+        print('cost =', cost)
         
-        if c_l <= y:
-            mask = G <= y
-            failure_probability = p_0 ** (l-1) * np.mean(mask) / denominator
+        G, thetas, c_l = compute_cl(G, thetas, N, y, p_0, l, L)
+        print('c', l, '= ', c_l)
+        
+        if c_l < 0:
+            p_l = len(G) / N
+            failure_probability = p_0 ** (l-1) * p_l / denominator
             return failure_probability, cost
         
-        mask = G <= c_l
-        thetas = thetas[mask][:N0]
-        G = G[mask][:N0]
+        _, G_l, add_cost = sampling(M, N, G, thetas, c_l, l, L, gamma, burn_in=burn_in)
+        cost += add_cost
+        print('cost =', cost)
         
-        _, G_l = generate_samples(thetas, G, N, gamma, c_l, l, burn_in)
-        cost += N + (burn_in - 1) * N0
-        
-        mask = G_l <= c_l_1
+        mask = np.array(G_l) <= c_l_1
         denominator *= np.mean(mask)
-        
-    # For l = L
+        print('denominator =', denominator)
+    
+    # l = L
     c_L_1 = c_l
-    thetas, G = generate_samples(thetas, G, N, gamma, c_l, L, burn_in)
-    cost += N + (burn_in - 1) * N0
+    thetas, G, add_cost = sampling(M, N, G, thetas, c_l, L, L, gamma, burn_in=burn_in)
+    cost += add_cost
+    print('cost =', cost)
     
-    mask = G <= y
-    thetas = thetas[mask]
-    G = G[mask]
-    p_L = np.mean(mask)
+    G, thetas, c_L = compute_cl(G, thetas, N,  y, p_0, L, L)
+    print('c_L =', c_L)
+    print('len(G) =', len(G))
+    p_L = len(G) / N
     
-    _, G_L = generate_samples(thetas, G, N, gamma, y, L, burn_in)
-    cost += N + (burn_in - 1) * N0
+    _, G_L, add_cost = sampling(M, N, G, thetas, y, L, L, gamma, burn_in=burn_in)
+    cost += add_cost
+    print('cost =', cost)
     
-    mask = G_L < c_L_1
+    mask = np.array(G_L) <= c_L_1
     denominator *= np.mean(mask)
     
     failure_probability = p_0 ** (L-1) * p_L / denominator
@@ -225,12 +191,15 @@ def mle_sr(gamma, y, p_0, N, L, burn_in):
     return failure_probability, cost
 
 if __name__ == "__main__":
-    L = 5
-    gamma = 0.5
+    p_0 = 0.25
+    M = 150
+    N = 1000
+    gamma = 0.25
     y = 0
-    p_0 = 0.1
-    N = 100
-    burn_in = 0
-    failure_probability, cost = mle_sr(gamma, y, p_0, N, L, burn_in)
-    print("The failure probability is: ", failure_probability)
-    print("The cost is: ", cost)
+    L = 8
+    burn_in = 10
+    
+    np.random.seed(8)
+    p_f, cost = mle_sr(gamma, y, p_0, M, N, L, burn_in)
+    print("The probability of failure is: {:.2e}".format(p_f))
+    # print("The cost is: ", cost)
