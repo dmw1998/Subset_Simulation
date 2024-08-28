626cd6455ca2711a8043ffdaa5d4c865b407e1d0
dmw <miaowen.dong@tum.de>
2024-08-28 21:16:56 +0800

    11

--- new.py
+++ new.py
@@ -36,7 +36,7 @@ def solving_pde(theta, l):
     import os
     os.environ["PETSC_OPTIONS"] = "-log_view ascii:out.log"
     
-    n_grid = int(64 * 2 ** (l-1))
+    n_grid = int(32 * 2 ** l-1)
     a_x = kl_expan(theta)
     
     # Create the mesh and define function space
@@ -69,104 +69,119 @@ def solving_pde(theta, l):
     
     return 0.535 - u_h(1)
 
-def compute_cl(G, thetas, N, p_0, l, L):
+def compute_cl(G, thetas, N, y, p_0, l, L):
     
-    sorted_indices = sorted(range(N), key=lambda k: G[k])
+    sorted_indices = sorted(range(N), key=lambda k: G[k], reverse=False)
     sorted_G = [G[i] for i in sorted_indices]
     sorted_thetas = [thetas[i] for i in sorted_indices]
     
     N_0 = int(p_0 * N)
     c_l = sorted_G[N_0-1]
     if c_l < 0 or l == L:
-        G = [g for g in G if g < 0]
+        G = [g for g in G if g >= 0]
     else:
         G = sorted_G[:N_0]
         thetas = sorted_thetas[:N_0]
     
     return G, thetas, c_l
 
-def sampling(N, G, thetas, c_l, l, gamma, burn_in=0):
+def sampling(M, N, G, thetas, c_l, l, L, gamma, burn_in=0):
     
-    M = len(thetas[0])
     N0 = len(G)
-    L_b = burn_in * N0
-    cost = 0
+    L_b = burn_in * (N0 - 1)
+    cost = np.zeros(L)
     
     for i in range(N + (burn_in - 1) * N0):
         theta_new = 0.8 * thetas[i] + np.sqrt(1 - 0.8 ** 2) * np.random.normal(0, 1, M)
         G_new = solving_pde(theta_new, 1)
-        cost += 1
-        tol = gamma
+        cost[0] += 1
+        tol = 1
         for j in range(2, l):
             if tol >= np.abs(G_new - c_l):
-                tol = gamma ** j
+                tol *= gamma
                 G_new = solving_pde(theta_new, j)
-                cost += j
+                cost[j-1] += 1
             else:
                 break
         
         if G_new <= c_l:
-            thetas = np.append(thetas, theta_new)
-            G = np.append(G, G_new)
+            thetas.append(theta_new)
+            G.append(G_new)
         else:
-            thetas = np.append(thetas, thetas[i])
-            G = np.append(G, G[i])
+            thetas.append(thetas[i])
+            G.append(G[i])
             
     return thetas[L_b:], G[L_b:], cost
 
-def mle_sr(gamma, y, p_0, N, L, burn_in):
+def mle_sr(gamma, y, p_0, M, N, L, burn_in):
     
     l = 1
-    thetas = [np.random.normal(0, 1, 150) for _ in range(N)]
+    thetas = [np.random.normal(0, 1, M) for _ in range(N)]
     G = [solving_pde(theta, 0) for theta in thetas]
-    cost = N * l
+    cost = np.zeros(L)
+    cost[0] = N
+    print('cost =', cost)
     
-    G, thetas, c_1 = compute_cl(G, thetas, N, p_0, l, L)
+    G, thetas, c_1 = compute_cl(G, thetas, N, y, p_0, l, L)
     print('c_1 = ', c_1)
+    # print('len(G) =', len(G))
     
     l = 2
-    thetas, G, add_cost = sampling(N, G, thetas, c_1, l, gamma, burn_in=0)
+    thetas, G, add_cost = sampling(M, N, G, thetas, c_1, l, L, gamma, burn_in=0)
     cost += add_cost
+    print('cost =', cost)
+    # print('len(thetas) =', len(thetas))
+    # print('len(G) =', len(G))
     
-    G, thetas, c_2 = compute_cl(G, thetas, N, p_0, l, L)
+    G, thetas, c_2 = compute_cl(G, thetas, N, y, p_0, l, L)
     print('c_2 = ', c_2)
     
-    _, G_2, add_cost = sampling(N, G, thetas, c_2, l, gamma, burn_in=0)
+    _, G_2, add_cost = sampling(M, N, G, thetas, c_2, l, L, gamma, burn_in=0)
     cost += add_cost
+    print('cost =', cost)
     
     mask = np.array(G_2) <= c_1
     denominator = np.mean(mask)
+    print('denominator =', denominator)
     
     # For l > 2, burn-in
     c_l = c_2
     for l in range(3, L):
         c_l_1 = c_l
-        thetas, G, add_cost = sampling(N, G, thetas, c_l, l, gamma, burn_in)
+        thetas, G, add_cost = sampling(M, N, G, thetas, c_l, l, L, gamma, burn_in=burn_in)
         cost += add_cost
-        G, thetas, c_l = compute_cl(G, thetas, N, p_0, l, L)
-        print('c_', l, ' = ', c_l)
+        print('cost =', cost)
+        
+        G, thetas, c_l = compute_cl(G, thetas, N, y, p_0, l, L)
+        print('c', l, '= ', c_l)
         
         if c_l < 0:
-            mask = np.array(G) <= 0
-            failure_probability = p_0 ** (l-1) * np.mean(mask) / denominator
+            p_l = len(G) / N
+            failure_probability = p_0 ** (l-1) * p_l / denominator
             return failure_probability, cost
         
-        _, G_l, add_cost = sampling(N, G, thetas, c_l, l, gamma, burn_in)
+        _, G_l, add_cost = sampling(M, N, G, thetas, c_l, l, L, gamma, burn_in=burn_in)
         cost += add_cost
+        print('cost =', cost)
         
         mask = np.array(G_l) <= c_l_1
         denominator *= np.mean(mask)
+        print('denominator =', denominator)
     
     # l = L
     c_L_1 = c_l
-    thetas, G, add_cost = sampling(N, G, thetas, c_l, L, gamma, burn_in)
+    thetas, G, add_cost = sampling(M, N, G, thetas, c_l, L, L, gamma, burn_in=burn_in)
     cost += add_cost
+    print('cost =', cost)
     
-    G, thetas, _ = compute_cl(G, thetas, N, p_0, L, L)
+    G, thetas, c_L = compute_cl(G, thetas, N,  y, p_0, L, L)
+    print('c_L =', c_L)
+    print('len(G) =', len(G))
     p_L = len(G) / N
     
-    _, G_L, add_cost = sampling(N, G, thetas, y, L, gamma, burn_in)
+    _, G_L, add_cost = sampling(M, N, G, thetas, y, L, L, gamma, burn_in=burn_in)
     cost += add_cost
+    print('cost =', cost)
     
     mask = np.array(G_L) <= c_L_1
     denominator *= np.mean(mask)
@@ -177,13 +192,14 @@ def mle_sr(gamma, y, p_0, N, L, burn_in):
 
 if __name__ == "__main__":
     p_0 = 0.25
+    M = 150
     N = 1000
-    gamma = 0.8
-    y = 0.535
-    L = 3
+    gamma = 0.25
+    y = 0
+    L = 8
     burn_in = 10
     
-    np.random.seed(0)
-    p_f, cost = mle_sr(gamma, y, p_0, N, L, burn_in)
+    np.random.seed(12)
+    p_f, cost = mle_sr(gamma, y, p_0, M, N, L, burn_in)
     print("The probability of failure is: {:.2e}".format(p_f))
-    print("The cost is: ", cost)
+    # print("The cost is: ", cost)
